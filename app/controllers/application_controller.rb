4753ec9053b5c87c4dc20d01d8c94685ff1eb196
jer-k <jeremy@carwoo.com>
2015-12-01 13:58:31 -0800

    Ignore .idea Null-session in Application controller so Swagger-UI works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
+
+  rescue_from(ActionController::ParameterMissing) do |parameter_missing_exception|
+    @error = "The #{parameter_missing_exception.param} parameter is missing"
+    render 'shared/error', status: :bad_request
+  end
 end
